876231e0590afa7f6040ff4aafdc1f2688a9e6d9
sergiomalagonmartin <smalagonmartin@gmail.com>
2022-03-07 01:09:50 +0100

    Update const.ts

--- lib/const.ts
+++ lib/const.ts
@@ -1,7 +1,7 @@
-export const API_URL: string = 'http://localhost/logrocho/logrocho-backend/index.php/api/';
+//export const API_URL: string = 'http://localhost/logrocho/logrocho-backend/index.php/api/';
 
-//export const API_URL: string = 'https://sergiomalagon.ociobinario.com/index.php/api/';
+export const API_URL: string = 'https://sergiomalagon.ociobinario.com/index.php/api/';
 
-export const IMG_URL: string = 'http://localhost/logrocho/logrocho-backend/img/';
+//export const IMG_URL: string = 'http://localhost/logrocho/logrocho-backend/img/';
 
-//export const IMG_URL: string = 'https://sergiomalagon.ociobinario.com/img/';
+export const IMG_URL: string = 'https://sergiomalagon.ociobinario.com/img/';
